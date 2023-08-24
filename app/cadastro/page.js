84766b5acf1541cccfa2135511c686936ff83bcd
NICOLE LICYANE VARELA DA SILVA <nicole_lv_silva@estudante.sesisenai.org.br>
2023-08-24 09:53:28 -0300

    first commit

---
+++ app/cadastro/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+import Image from "next/image";
+import Link from 'next/link'
+
+
+export default function Cadastro() {
+  return (
+    <main>
+      OIIIIIIII CADASTRO</main>
+  );
+}
