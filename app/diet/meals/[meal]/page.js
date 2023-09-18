eb8c45fb040a3f0023e376450f69ba99d15fb2bf
GUSTAVO DE SOUZA BRAGA <gustavo_s_braga@estudante.sesisenai.org.br>
2023-09-18 12:26:03 -0300

    commit

--- app/diet/meals/[meal]/page.js
+++ app/diet/meals/[meal]/page.js
@@ -3,14 +3,16 @@
 import Link from "next/link";
 import { IoIosArrowBack } from "react-icons/io";
 import { AiFillYoutube } from "react-icons/ai";
+import { TbWorldSearch } from "react-icons/tb";
 import Image from "next/image";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Loading from "@/components/Loading";
 
 export default function Page({ params }) {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [meal, setMeal] = useState({});
+  const [ingredientsList, setIngredientsList] = useState([]);
 
   const href = `../../diet/category/${meal.strCategory}`;
 
@@ -20,11 +22,29 @@ export default function Page({ params }) {
       .then(function (response) {
         setLoading(false);
         setMeal(response.data.meals[0]);
-        console.log(response.data.meals[0]);
+        let ingredients = [];
+        let refactoredIngredients = [];
+        for (const item in response.data.meals[0]) {
+          if (item.startsWith("strIngredient") && response.data.meals[0][item] != " " && response.data.meals[0][item])
+            ingredients.push(response.data.meals[0][item]);
+        }
+        for (const item in response.data.meals[0]) {
+          if (item.startsWith("strMeasure") && response.data.meals[0][item] != " " && response.data.meals[0][item])
+            refactoredIngredients.push({
+              name: ingredients[item.split("strMeasure")[1] - 1],
+              measure: response.data.meals[0][item],
+            });
+        }
+        setIngredientsList(refactoredIngredients);
       })
       .catch(function (error) {
         return error;
       });
+  };
+
+  const formatText = (text) => {
+    if (!text) return "Sem instruções";
+    return text.replace(/\\n/g, "<br>");
   };
 
   useEffect(() => {
@@ -48,6 +68,7 @@ export default function Page({ params }) {
               src={meal.strMealThumb}
               width={"100"}
               height={"100"}
+              alt="Imagem da comida"
             ></Image>
             <div className="w-full h-32 object-cover rounded-b-3xl bg-black opacity-60 absolute"></div>
             <div className="flex items-center gap-4 absolute left-5 bottom-14 text-white">
@@ -57,14 +78,40 @@ export default function Page({ params }) {
               <p className="text-2xl font-semibold">{meal.strMeal}</p>
             </div>
           </div>
-          <div className="p-4">
-            <div className="w-fit p-2 rounded-md bg-[#dddddd] flex gap-2">
-              <AiFillYoutube className="w-6 h-6 text-red-600"></AiFillYoutube>
-              <div>Abrir receita no YouTube</div>
+          <div className="p-4 flex gap-4 flex-wrap">
+            {meal.strYoutube ? (
+              <Link href={meal.strYoutube} target="_blank">
+                <div className="w-fit p-2 rounded-md bg-[#F4F4F4] flex gap-2 cursor-pointer hover:bg-[#dddddd] transition-all active:scale-90">
+                  <AiFillYoutube className="w-6 h-6 text-red-600"></AiFillYoutube>
+                  <div>Abrir receita no YouTube</div>
+                </div>
+              </Link>
+            ) : (
+              false
+            )}
+            {meal.strSource ? (
+              <Link href={meal.strSource} target="_blank">
+                <div className="w-fit p-2 rounded-md bg-[#F4F4F4] flex gap-2 cursor-pointer hover:bg-[#dddddd] transition-all active:scale-90">
+                  <TbWorldSearch className="w-6 h-6 text-blue-600"></TbWorldSearch>
+                  <div>Abrir receita no Navegador</div>
+                </div>
+              </Link>
+            ) : (
+              false
+            )}
+            <div className="flex w-full p-4 gap-2 flex-col overflow-x-scroll border border-gray-300 rounded-md">
+              <p className="font-semibold">Instruções</p>
+              <pre className="tracking-tighter">{formatText(meal.strInstructions)}</pre>
             </div>
-            <div className="w-fit p-2 rounded-md bg-[#dddddd] flex gap-2">
-              <AiFillYoutube className="w-6 h-6 text-red-600"></AiFillYoutube>
-              <div>Abrir receita no Navegador</div>
+            <div className="flex w-full p-4 gap-2 flex-col">
+              <p className="font-semibold">Ingredientes</p>
+              {ingredientsList.map((ingredient) => {
+                return (
+                  <li className="font-semibold" key={ingredient.name}>
+                    {ingredient.name.toUpperCase()}: <span className="font-normal">{ingredient.measure}</span>
+                  </li>
+                );
+              })}
             </div>
           </div>
         </div>
--- components/mobile.js
+++ components/mobile.js
@@ -5,7 +5,7 @@ export default function Mobile({ children }) {
   return (
     <main className="flex">
       <div className="justify-center flex-col mx-auto flex">
-        <div className="border-2 border-[#00000]">{children}</div>
+        <div className="border-x-2 border-[#00000]">{children}</div>
       </div>
     </main>
   );
